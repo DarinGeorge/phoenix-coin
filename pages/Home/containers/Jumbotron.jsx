cf33e0ff137b76d2279a5cc742df5e6278ba476a
DarinGeorge <Darin.George30@gmail.com>
2022-04-03 21:40:53 -0400

    added loading lottie before bugfix and testing

--- pages/Home/containers/Jumbotron.jsx
+++ pages/Home/containers/Jumbotron.jsx
@@ -1,16 +1,30 @@
 import {useContext} from 'react';
+import Lottie from 'lottie-react';
+
 import {SolanaContext} from '../../../context/solana';
 import {styles} from '../../../styles/containers/Home/Jumbotron.tailwind';
 import ConnectButton from '../../../components/ConnectButton';
+import coinLoader from '../../../assets/loading-coin.json';
 
 export default function Jumbotron({supported}) {
-  const {connected} = useContext(SolanaContext);
+  const {connected, loading} = useContext(SolanaContext);
 
   return (
     <div className={`${styles.wrapper} ${connected && styles.isConnected}`}>
       <div>
-        <h1 className={styles.heading}>Mint Authorizer</h1>
-        <p className={styles.poweredBy}>powered by Solana Blockchain</p>
+        {connected && loading ? (
+          <Lottie
+            animationData={coinLoader}
+            autoplay
+            loop
+            style={{position: 'absolute', top: -17, left: -10, width: 100, height: 110}}
+          />
+        ) : (
+          <>
+            <h1 className={styles.heading}>Mint Authorizer</h1>
+            <p className={styles.poweredBy}>powered by Solana Blockchain</p>
+          </>
+        )}
       </div>
 
       {supported && <ConnectButton />}
